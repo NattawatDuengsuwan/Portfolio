68b2a7cd933e2c380176ff0ebedc9fba7553a87b
NattawatDuengsuwan <nattawatpapa13@gmail.com>
2024-11-06 23:51:48 +0700

    สิ้นใจ
    สิ้นใจ

---
+++ Portfolio-Website/src/data/constants.js
@@ -0,0 +1,197 @@
+export const Bio = {
+  name: "Nattawat Duegsuwan",
+  roles: [
+    "Web Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a student in Electronics Engineering (Computer) with a strong interest in studying website design. I am currently seeking internship opportunities to apply my technical skills in real-world projects, as well as to enhance and further develop my capabilities in this field.",
+  github: "https://github.com/NattawatDuengsuwan",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  
+  insta: "https://www.instagram.com/_immpxa/",
+  facebook: "https://www.facebook.com/profile.php?id=100012959926382",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://midu.dev/images/tags/git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s",
+    role: "Portfolio by React",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Oct 2024 - Present",
+    desc: "his small-scale project involves designing and developing a portfolio website to comprehensively showcase work and other important information. The website will be meticulously designed with UX/UI to ensure it is visually appealing and user-friendly.",
+    skills: [
+      "React",
+      "CSS",
+      "Git",
+      "Node JS",
+      "Vercel",
+      "JavaScript",
+      "XML",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://i.pinimg.com/originals/73/3b/a2/733ba204bf3324080a778b1c84f71d87.jpg",
+    role: "Flutter Project",
+    company: "MOBILE & CLOUD COMPUTING.",
+    date: "Dec 2023 - Mar 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/th/thumb/b/be/Seal_of_KMUTNB.svg/300px-Seal_of_KMUTNB.svg.png",
+    school: "King Mongkut's University of Technology North Bangkok",
+    date: "July 2021 - Present",
+    grade: "2.07 GPAX",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Engineering at King Mongkut's University of Technology North Bangkok.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmIw8D_yPuFkiGv82lTwIxEBLsj6BSr2eRzA&s",
+    school: "Samutprakan Technical College",
+    date: "Apr 2019 - Apr 2021",
+    grade: "2.97 GPAX",
+    desc: "I graduated from Samut Prakan Technology College, majoring in Electronics.",
+    degree: "Electronics",
+  },
+];
+
+
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2021, text: "Started my own platform" },
+];
